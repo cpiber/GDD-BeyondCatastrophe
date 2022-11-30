c96c0f2fb6fd252d0f3d19e5c5df61a24db7263c
Lukas Mathä <yato16@student.tugraz.at>
2022-11-30 15:11:14 +0100

    [#2] Add a testing scene with borders

--- Unity/Assets/Scripts/PlayerController.cs
+++ Unity/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,24 @@
+using DefaultNamespace;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    void Update() {
-        var move = Input.GetAxis("Horizontal");
-        this.GetComponent<Rigidbody2D>().velocity = new Vector2(move * 5, 0);
+    void Update()
+    {
+        var move_x = Input.GetAxis("Horizontal");
+        var move_y = Input.GetAxis("Vertical");
+        this.GetComponent<Rigidbody2D>().velocity = new Vector2(move_x * 5, move_y * 5);
     }
 
     void OnCollisionEnter2D(Collision2D collider) {
         var s = collider.gameObject.GetComponent<SceneObjectState>();
-        Debug.Log(s);
-        if (s) s.Destroy();
+        var border = collider.gameObject.GetComponent<Border>();
+        if (border)
+        {
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+        }
+        else if (s) s.Destroy();
         else Destroy(collider.gameObject);
+        
     }
 }
