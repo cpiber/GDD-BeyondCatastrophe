2a0b13df9e8b52d7985894798a29eb2fe2910827
Constantin Piber <constantin.piber@student.tugraz.at>
2022-12-16 12:19:59 +0100

    Hide Quit only in WebGL, build

--- Unity/Assets/Scripts/MainMenu.cs
+++ Unity/Assets/Scripts/MainMenu.cs
@@ -6,6 +6,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject quitButton;
+
+    void Start() {
+#if UNITY_WEBGL
+        quitButton.SetActive(false);
+#endif
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("MainGameScene");
