65ff377a412994db050a89898c2af432130d0d3c
thenextmz <mario.comanici@gmx.at>
2023-02-06 19:17:57 +0100

    Play Audio with dialogue setup

--- Unity/Assets/Scripts/DialogueSystem.cs
+++ Unity/Assets/Scripts/DialogueSystem.cs
@@ -11,11 +11,20 @@ public class DialogueSystem : GenericSingleton<DialogueSystem>
     [SerializeField] string[] lines = {"Hello this is a test", "Test line number 2", "Test Line number three"};
     [SerializeField] float speed = 0.1f;
     private int lineIndex;
-
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip[] clips;
+    [SerializeField] float volume = 1f;
+  
     void Start(){
         gameObject.SetActive(false);
+        /* 
+        // -> Call Dialogue system like this
+        this.clips = new AudioClip[] {Resources.Load<AudioClip>("Audio/test1"), 
+                                      Resources.Load<AudioClip>("Audio/test1"),
+                                      Resources.Load<AudioClip>("Audio/test1")};
         // DialogueSystem.the().StartDialogue(new string[] {"Hello from Bedtime", "My god please work"});
-        // StartDialogue(this.lines);
+        StartDialogue(this.lines, this.clips);
+        */
     }
 
     void OnClick (InputValue input) {
@@ -31,7 +40,6 @@ public class DialogueSystem : GenericSingleton<DialogueSystem>
     }
 
 
-
     void CompleteDialogue() {
         StopAllCoroutines();
         if (textComponent.text == lines[lineIndex]){
@@ -41,15 +49,19 @@ public class DialogueSystem : GenericSingleton<DialogueSystem>
         }
     }
 
-    public void StartDialogue(string[] lines){
+    public void StartDialogue(string[] lines, AudioClip[] clips){
         gameObject.SetActive(true);
         textComponent.text = "";
         this.lines = lines;
+        this.clips = clips;
         lineIndex = 0;
         StartCoroutine(TypeLine());
     }
 
     IEnumerator TypeLine(){
+        // TODO: only play audio if flag set in menue
+        audioSource.Stop();
+        audioSource.PlayOneShot(clips[lineIndex], volume);
         for (int i = 0; i < lines[lineIndex].Length; i++){
             textComponent.text += lines[lineIndex][i];
             yield return new WaitForSeconds(speed);
@@ -63,6 +75,8 @@ public class DialogueSystem : GenericSingleton<DialogueSystem>
             StartCoroutine(TypeLine());
         } else {
             gameObject.SetActive(false);
+            // TODO: only stop if audio flag set
+            audioSource.Stop();
         }
     }
 }
