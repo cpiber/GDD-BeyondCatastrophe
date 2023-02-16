c7b8426706487a39d2c4f14ddaafffce011cfcd2
Constantin Piber <constantin.piber@student.tugraz.at>
2023-02-16 17:54:29 +0100

    Change GlobalSceneState to use ScriptableObject
    ScriptableObject should allow for better serialization

--- Unity/Assets/Scripts/SceneObjectState.cs
+++ Unity/Assets/Scripts/SceneObjectState.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneObjectState : MonoBehaviour
 {
     [SerializeField] string globalKey;
     
     void Start() {
-        var state = GlobalSceneState.the().getState(globalKey, transform.position);
+        var state = GlobalSceneState.the().getState(globalKey);
+        if (state == null) return;
         if (!state.exists) Destroy(gameObject);
         else transform.position = state.position;
+        Debug.Log($"Loading {globalKey}: exists? {state.exists}");
     }
 
     void OnDestroy() {
@@ -20,4 +23,12 @@ public class SceneObjectState : MonoBehaviour
         Debug.Log($"Destorying completely {globalKey}");
         Destroy(gameObject);
     }
+
+#if UNITY_EDITOR
+    void OnValidate() {
+        if (globalKey == "") {
+            globalKey = SceneManager.GetActiveScene().name + "." + name;
+        }
+    }
+#endif
 }
