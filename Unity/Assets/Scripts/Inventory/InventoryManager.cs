0f173ab301120d96735c2124ae4c97976d7508f6
Jan-Heliodor Tscherko <j.tscherko@student.tugraz.at>
2023-02-18 16:03:50 +0100

    moving apple into free slot in chest and adding more of it in the chest works #11

--- Unity/Assets/Scripts/Inventory/InventoryManager.cs
+++ Unity/Assets/Scripts/Inventory/InventoryManager.cs
@@ -10,11 +10,13 @@ public class InventoryManager : GenericSingleton<InventoryManager>
     [SerializeField] InventoryUIManager uiManager;
     private GameObject selectedItemToMove = null;
 
-    void Start() {
+    void Start()
+    {
         GameObject itemsObjects = GameObject.Find("Items");
         items = new SerializableDictionary<string, List<Item>>();
 
-        foreach (Transform item in itemsObjects.transform) {
+        foreach (Transform item in itemsObjects.transform)
+        {
             List<Item> chestAndBagItemList = new List<Item>();
             chestAndBagItemList.Add(item.gameObject.GetComponent<Item>());
             items[item.gameObject.name] = chestAndBagItemList;
@@ -22,7 +24,8 @@ public class InventoryManager : GenericSingleton<InventoryManager>
 
         itemsObjects = GameObject.Find("ChestItems");
 
-        foreach (Transform item in itemsObjects.transform) {
+        foreach (Transform item in itemsObjects.transform)
+        {
             List<Item> test = items[item.gameObject.name];
             List<Item> test2 = items[item.gameObject.name];
 
@@ -31,8 +34,10 @@ public class InventoryManager : GenericSingleton<InventoryManager>
     }
 
     // list is 0 indexed (three weapons => 0,1,2 slot)
-    public void UseEquippedItem(int slotIndex) {
-        if (uiManager.EquippedInventoryItems.childCount <= slotIndex) {
+    public void UseEquippedItem(int slotIndex)
+    {
+        if (uiManager.EquippedInventoryItems.childCount <= slotIndex)
+        {
             Debug.LogWarning("Such an equiment slot does not exist!");
             return;
         }
@@ -42,37 +47,45 @@ public class InventoryManager : GenericSingleton<InventoryManager>
         itemSlot.SetCount();
 
         // if item has no use left => reset item slot to no item
-        if (!itemToUse.IsReusable()) {
+        if (!itemToUse.IsReusable())
+        {
             NonPermanentItem item = (NonPermanentItem)itemToUse;
             // if item is out of usage => remove item from inventory
-            if (!item.CanBeUsed()) {
+            if (!item.CanBeUsed())
+            {
                 itemSlot.ResetSlot();
             }
-        } 
-    }   
+        }
+    }
 
-    public void UseBag() {
+    public void UseBag()
+    {
         InventoryUIManager.the().ToggleBagUI();
         UnselectButtons();
     }
 
-    public void UseChest() {
+    public void UseChest()
+    {
         InventoryUIManager.the().ToggleChestUI();
         UnselectButtons();
     }
 
-    public void UseItem(string itemName) {
-        if (!items.ContainsKey(itemName)) {
+    public void UseItem(string itemName)
+    {
+        if (!items.ContainsKey(itemName))
+        {
             Debug.LogWarning($"Such an item ({itemName}) does not exist!");
             return;
         }
         // zero index for bag items
         Item itemToUse = items[itemName][0];
         itemToUse.UseItem();
-    }  
+    }
 
-    public bool AddItem(string itemName, int slotIndex = 0, int fromIndex = 0) {
-        if (!items.ContainsKey(itemName)) {
+    public bool AddItem(string itemName, int slotIndex = 0, int fromIndex = 0)
+    {
+        if (!items.ContainsKey(itemName))
+        {
             Debug.LogWarning($"Such an item ({itemName}) does not exist!");
             return false;
         }
@@ -81,37 +94,46 @@ public class InventoryManager : GenericSingleton<InventoryManager>
         Transform itemFound = null;
         Transform firstFreeSlot = null;
         IEnumerable<Transform> slots = fromIndex == 0 ? GetBagItemSlots() : GetChestSlots();
-        foreach(Transform itemSlot in slots) {
+        foreach (Transform itemSlot in slots)
+        {
             InventorySlot slot = itemSlot.GetComponent<InventorySlot>();
-            if (slot.GetSlotItemName() == itemName) {
+            if (slot.GetSlotItemName() == itemName)
+            {
                 itemFound = itemSlot;
                 break;
             }
-            if (slot.IsSlotEmpty() && firstFreeSlot == null) {
+            if (slot.IsSlotEmpty() && firstFreeSlot == null)
+            {
                 firstFreeSlot = itemSlot;
             }
         }
 
-        if (itemFound) {
+        if (itemFound)
+        {
             InventorySlot slot = itemFound.GetComponent<InventorySlot>();
             Item itemScript = slot.GetItem();
-            if (itemScript is NonPermanentItem) {
+            if (itemScript is NonPermanentItem)
+            {
                 NonPermanentItem nonPermanentItem = (NonPermanentItem)itemScript;
                 nonPermanentItem.IncreaseItemCount();
                 slot.SetCount();
                 return true;
-            } else {
+            }
+            else
+            {
                 return false;
             }
-        } 
-        
+        }
+
         firstFreeSlot.GetComponent<InventorySlot>().SetSlot(itemFrom);
         return true;
     }
 
 
-    public (Item, Item) AddItemInOtherLevel(string itemName, int slotIndex = 0, int fromIndex = 0) {
-        if (!items.ContainsKey(itemName)) {
+    public (Item, Item) AddItemInOtherLevel(string itemName, int slotIndex = 0, int fromIndex = 0)
+    {
+        if (!items.ContainsKey(itemName))
+        {
             Debug.LogWarning($"Such an item ({itemName}) does not exist!");
             return (null, null);
         }
@@ -120,39 +142,94 @@ public class InventoryManager : GenericSingleton<InventoryManager>
         Transform itemFound = null;
         Transform firstFreeSlot = null;
         IEnumerable<Transform> slots = fromIndex == 0 ? GetBagItemSlots() : GetChestSlots();
-        foreach(Transform itemSlot in slots) {
+        foreach (Transform itemSlot in slots)
+        {
             InventorySlot slot = itemSlot.GetComponent<InventorySlot>();
-            if (slot.GetSlotItemName() == itemName) {
+            if (slot.GetSlotItemName() == itemName)
+            {
                 itemFound = itemSlot;
                 break;
             }
-            if (slot.IsSlotEmpty() && firstFreeSlot == null) {
+            if (slot.IsSlotEmpty() && firstFreeSlot == null)
+            {
                 firstFreeSlot = itemSlot;
             }
         }
 
-        if (itemFound) {
+        if (itemFound)
+        {
             InventorySlot slot = itemFound.GetComponent<InventorySlot>();
             Item itemScript = slot.GetItem();
-            if (itemScript is NonPermanentItem) {
-                NonPermanentItem nonPermanentItem = (NonPermanentItem)itemScript;
-                NonPermanentItem nonPermanentItemTo = (NonPermanentItem)itemTo;
-                nonPermanentItemTo.IncreaseItemCount();
-                nonPermanentItem.DecreaseItemCount();
-                slot.SetCount();
+
+            // handle case if both items are non permanent
+            if (itemScript is NonPermanentItem && itemTo is NonPermanentItem)
+            {
+                itemScript = HandleNonPermanentItem(itemScript, false, fromIndex);
+                itemTo = HandleNonPermanentItem(itemTo, true, slotIndex);
                 return (itemTo, itemScript);
-            } else {
-                return (null, null);
             }
-        } 
-        
+            else if (itemScript is NonPermanentItem)
+            {
+                itemScript = HandleNonPermanentItem(itemScript, false, fromIndex);
+                itemTo = HandlePermanentItem(itemTo, slotIndex);
+                return (itemTo, itemScript);
+            }
+            else if (itemTo is NonPermanentItem) {
+                HandleNonPermanentItem(itemScript, true, fromIndex);
+            } else 
+            {
+                // todo must be done right
+                return (items["EmptyItem"][fromIndex], items["EmptyItem"][fromIndex]);
+            }
+        }
+
         firstFreeSlot.GetComponent<InventorySlot>().SetSlot(itemTo);
         return (itemTo, items["EmptyItem"][0]);
     }
 
+    public Item HandlePermanentItem(Item item, int index)
+    {
+        return items["EmptyItem"][index];
+    }
 
-    public bool RemoveItem(string itemName, int slotIndex = 0) {
-        if (!items.ContainsKey(itemName)) {
+    public Item HandleNonPermanentItem(Item item, bool increase, int index)
+    {
+        NonPermanentItem nonPermanentItem = (NonPermanentItem)item;
+        if (increase)
+        {
+            nonPermanentItem.IncreaseItemCount();
+        }
+        else
+        {
+            nonPermanentItem.DecreaseItemCount();
+            if (nonPermanentItem.Count() < 1)
+            {
+                return items["EmptyItem"][index];
+            }
+        }
+        return item;
+    }
+
+    public (Item, Item) SwapNonPermanentItems(Item itemScript, Item itemTo, InventorySlot slot, int fromIndex)
+    {
+        NonPermanentItem nonPermanentItem = (NonPermanentItem)itemScript;
+        NonPermanentItem nonPermanentItemTo = (NonPermanentItem)itemTo;
+
+        nonPermanentItem.DecreaseItemCount();
+        if (nonPermanentItem.Count() < 1)
+        {
+            itemScript = items["EmptyItem"][fromIndex];
+        }
+        slot.SetCount();
+        return (itemTo, itemScript);
+    }
+
+
+
+    public bool RemoveItem(string itemName, int slotIndex = 0)
+    {
+        if (!items.ContainsKey(itemName))
+        {
             Debug.LogWarning($"Such an item ({itemName}) does not exist!");
             return false;
         }
@@ -161,42 +238,53 @@ public class InventoryManager : GenericSingleton<InventoryManager>
         Transform itemFound = null;
 
         IEnumerable<Transform> slots = slotIndex == 0 ? GetBagItemSlots() : GetChestSlots();
-        foreach(Transform itemSlot in slots) {
+        foreach (Transform itemSlot in slots)
+        {
             InventorySlot slot = itemSlot.GetComponent<InventorySlot>();
-            if (slot.GetSlotItemName() == itemName) {
+            if (slot.GetSlotItemName() == itemName)
+            {
                 itemFound = itemSlot;
                 break;
             }
         }
 
-        if (itemFound) {
+        if (itemFound)
+        {
             InventorySlot slot = itemFound.GetComponent<InventorySlot>();
             Item itemScript = slot.GetItem();
-            if (itemScript is NonPermanentItem) {
+            if (itemScript is NonPermanentItem)
+            {
                 NonPermanentItem nonPermanentItem = (NonPermanentItem)itemScript;
                 nonPermanentItem.ResetItemCount();
                 slot.SetCount();
                 return true;
-            } else {
+            }
+            else
+            {
                 slot.SetSlot(items["EmptyItem"][slotIndex]);
             }
-        } 
+        }
 
         return true;
     }
 
 
-    public void SelectItemToMove(GameObject itemToMove) {
-        if (!uiManager.IsUIOpen) {
+    public void SelectItemToMove(GameObject itemToMove)
+    {
+        if (!uiManager.IsUIOpen)
+        {
             UnselectButtons();
             return;
         }
 
-        if (selectedItemToMove == null) {
+        if (selectedItemToMove == null)
+        {
             // if first item is selected
             selectedItemToMove = itemToMove;
             uiManager.OnSelectSlotForMove(itemToMove);
-        } else {
+        }
+        else
+        {
             // if second item is selected => swap
             InventorySlot firstItemSlot = selectedItemToMove.GetComponent<InventorySlot>();
             InventorySlot secondItemSlot = itemToMove.GetComponent<InventorySlot>();
@@ -206,43 +294,45 @@ public class InventoryManager : GenericSingleton<InventoryManager>
 
             // check if item is swappable
 
-            if (firstItemSlot.name.Contains("Armor") && !secondItem.IsArmor()) {
+            if (firstItemSlot.name.Contains("Armor") && !secondItem.IsArmor())
+            {
                 UnselectButtons();
                 uiManager.OnAttemptSwapItems(selectedItemToMove, itemToMove);
                 return;
-            } 
-            if (secondItemSlot.name.Contains("Armor") && !firstItem.IsArmor()) {
+            }
+            if (secondItemSlot.name.Contains("Armor") && !firstItem.IsArmor())
+            {
                 UnselectButtons();
                 uiManager.OnAttemptSwapItems(selectedItemToMove, itemToMove);
                 return;
-            } 
+            }
 
             // get name so we can check if it is a bag item or a chest item
             string firstSlotName = firstItemSlot.gameObject.name;
             string secondSlotName = secondItemSlot.gameObject.name;
 
             // check if items are the same
-            if (firstItem.GetItemName() == secondItem.GetItemName()) {
-                // add one instance to the other if there are several uses possible
+            /*  if (firstItem.GetItemName() == secondItem.GetItemName()) {
+                 // add one instance to the other if there are several uses possible
 
 
-                AddItem(secondItem.GetItemName());
+                 AddItem(secondItem.GetItemName());
 
-                //check if it is non permanent item 
-                if (firstItem is NonPermanentItem) {
-                    NonPermanentItem nonPermanentItem = (NonPermanentItem)firstItem;
-                    nonPermanentItem.DecreaseItemCount();
+                 //check if it is non permanent item 
+                 if (firstItem is NonPermanentItem) {
+                     NonPermanentItem nonPermanentItem = (NonPermanentItem)firstItem;
+                     nonPermanentItem.DecreaseItemCount();
 
-                    // if no item or use is left after stacking => set slot to empty
-                    if (nonPermanentItem.Count() == 0) {
-                        if (firstItemSlot.GetName().Contains("BagItem")) {
-                            firstItemSlot.SetSlot(items["EmptyItem"][0]);
-                        } else {
-                            firstItemSlot.SetSlot(items["EmptyItem"][1]);
-                        }
-                    }
-                }
-            } 
+                     // if no item or use is left after stacking => set slot to empty
+                     if (nonPermanentItem.Count() == 0) {
+                         if (firstItemSlot.GetName().Contains("BagItem")) {
+                             firstItemSlot.SetSlot(items["EmptyItem"][0]);
+                         } else {
+                             firstItemSlot.SetSlot(items["EmptyItem"][1]);
+                         }
+                     }
+                 }
+             }  */
 
 
             Item itemTo = items["EmptyItem"][0];
@@ -250,14 +340,17 @@ public class InventoryManager : GenericSingleton<InventoryManager>
 
             // swap from bag to chest or from chest to bag
             if ((!firstItemSlot.GetName().Contains("BagItem") && secondItemSlot.GetName().Contains("BagItem"))
-                ) {
+                )
+            {
                 (itemTo, itemFrom) = AddItemInOtherLevel(firstItem.GetItemName(), 0, 1);
 
-            } else if (firstItemSlot.GetName().Contains("BagItem") && !secondItemSlot.GetName().Contains("BagItem")) {
+            }
+            else if (firstItemSlot.GetName().Contains("BagItem") && !secondItemSlot.GetName().Contains("BagItem"))
+            {
                 (itemTo, itemFrom) = AddItemInOtherLevel(firstItem.GetItemName(), 1, 0);
             }
 
-            firstItemSlot.SetSlot(firstItem);
+            firstItemSlot.SetSlot(itemFrom);
             secondItemSlot.SetSlot(itemTo);
             uiManager.UpdateArmorStats();
             uiManager.OnAttemptSwapItems(selectedItemToMove, itemToMove);
@@ -265,40 +358,49 @@ public class InventoryManager : GenericSingleton<InventoryManager>
         }
     }
 
-    public void UnselectButtons() {
+    public void UnselectButtons()
+    {
         EventSystem.current.SetSelectedGameObject(null);
         selectedItemToMove = null;
     }
 
     // TODO This does a lot of calls to GetComponent<>, which is slow
     //      For future performance improvements, we might want to cache this
-    private IEnumerable<Item> GetItemsFromSlot(Transform inv) {
-        foreach (Transform child in inv) {
+    private IEnumerable<Item> GetItemsFromSlot(Transform inv)
+    {
+        foreach (Transform child in inv)
+        {
             var slot = child.gameObject.GetComponent<InventorySlot>();
             yield return slot.GetItem();
         }
     }
 
-    public IEnumerable<Item> GetBagItems() {
+    public IEnumerable<Item> GetBagItems()
+    {
         return GetItemsFromSlot(uiManager.BagInventoryItems);
     }
-    public IEnumerable<Item> GetChestItems() {
+    public IEnumerable<Item> GetChestItems()
+    {
         return GetItemsFromSlot(uiManager.ChestInventoryItems);
     }
-    public IEnumerable<Item> GetArmorItems() {
+    public IEnumerable<Item> GetArmorItems()
+    {
         return GetItemsFromSlot(uiManager.ArmorInventoryItems);
     }
-    public IEnumerable<Item> GetEquippedItems() {
+    public IEnumerable<Item> GetEquippedItems()
+    {
         return GetItemsFromSlot(uiManager.EquippedInventoryItems);
     }
 
-    public IEnumerable<Transform> GetBagItemSlots() {
+    public IEnumerable<Transform> GetBagItemSlots()
+    {
         for (int i = 0; i < uiManager.BagInventoryItems.childCount; i++) yield return uiManager.BagInventoryItems.GetChild(i);
         for (int i = 0; i < uiManager.EquippedInventoryItems.childCount; i++) yield return uiManager.EquippedInventoryItems.GetChild(i);
         for (int i = 0; i < uiManager.ArmorInventoryItems.childCount; i++) yield return uiManager.ArmorInventoryItems.GetChild(i);
     }
 
-    public IEnumerable<Transform> GetChestSlots() {
+    public IEnumerable<Transform> GetChestSlots()
+    {
         for (int i = 0; i < uiManager.ChestInventoryItems.childCount; i++) yield return uiManager.ChestInventoryItems.GetChild(i);
     }
 }
